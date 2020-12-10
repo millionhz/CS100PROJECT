d15a3c7fbc31cbe1583bbf65f9793be93517ac70
millionhz <m.waqas.hamza@gmail.com>
2020-12-10 16:21:37 +0500

    user prompt added

--- ASCIIART/main.cpp
+++ ASCIIART/main.cpp
@@ -11,7 +11,7 @@
 using namespace cv;
 using namespace std;
 
-
+enum class parsing { SUCCESS, FAIL, PROMPT };
 
 /*
 Press any key to end the program
@@ -20,7 +20,7 @@ void anyKeyToExit()
 {
     cout << "Press Any Key To Exit...";
     char _ = _getch();
-    exit(-1);
+    exit(0);
 }
 
 /*
@@ -38,6 +38,19 @@ void printUserInput(int argc, char* argv[])
     cout << "\b " << endl;
 }
 
+/*
+Input and clean double quotes ("") from file path
+@return file path
+*/
+string cleanImagePath(string image_file)
+{
+    if (image_file.substr(0, 1) == "\"" && image_file.substr(image_file.length() - 1, 1) == "\"")
+    {
+        image_file = image_file.substr(1, image_file.length() - 2);
+    }
+
+    return image_file;
+}
 
 /*
 Prints help onto the console
@@ -54,30 +67,57 @@ void printHelp()
     cout << "Example: ./myimage.jpg 110 40 240 1 art.txt" << endl;
 }
 
+
+/*
+Prompt user for input
+*/
+void promptUserForInput(string& image_file, int& size, int& low, int& high, bool& black_bg, string& text_file)
+{
+    cout << "Input image file: "; getline(cin, image_file); image_file = cleanImagePath(image_file);
+
+    do
+    {
+        cout << "Input Size (>0): "; cin >> size;
+    } while (size < 1);
+
+    do
+    {
+        cout << "Input lower value for normalization (0-255): "; cin >> low;
+    } while (low < 1 && low > 255);
+
+    do
+    {
+        cout << "Input upper value for normalization (0-255): "; cin >> high;
+    } while (high < 1 && high > 255);
+
+    cout << "Produce image with black background config (0 = no | >=1 = yes): "; cin >> black_bg;
+
+    cin.ignore(1000, '\n');
+    cout << "Input text file path for output : "; getline(cin, text_file); text_file = cleanImagePath(text_file);
+
+}
+
 /*
 Validates the input arguments from the user
 @param argc: size of input argument array
 @param argv: input argument array
 @return true if successful and false otherwise
 */
-bool parseArgs(int argc, char* argv[])
+parsing parseArgs(int argc, char* argv[])
 {
-    if (argc == 7 && atoi(argv[2]) > 0 && atoi(argv[3]) >= 0 && atoi(argv[3]) < 255  && atoi(argv[4]) > 0 && atoi(argv[4]) <= 255 && atoi(argv[5]) >= 0 && atoi(argv[5]) <= 1)
-
+    if (argc == 7 && atoi(argv[2]) > 0 && atoi(argv[3]) >= 0 && atoi(argv[3]) < 255 && atoi(argv[4]) > 0 && atoi(argv[4]) <= 255 && atoi(argv[5]) >= 0 && atoi(argv[5]) <= 1)
     {
         cout << "Parse Successful..." << endl;
-        return true;
+        return parsing::SUCCESS;
     }
-    else if ( argc == 2 && ((string)argv[1] == "-h" || (string)argv[1] == "--help"))
+    else if (argc == 2 && ((string)argv[1] == "-h" || (string)argv[1] == "--help"))
     {
         printHelp();
-        return true;
+        return parsing::FAIL;
     }
     else if (argc == 1)
     {
-        printHelp();
-        return false;;
-
+        return parsing::PROMPT;
     }
     else
     {
@@ -86,7 +126,7 @@ bool parseArgs(int argc, char* argv[])
         printHelp();
 
         printUserInput(argc, argv);
-        return false;
+        return parsing::FAIL;
     }
 }
 
@@ -99,32 +139,8 @@ Mat readImage(string image_file)
 {
     Mat image = imread(image_file, IMREAD_GRAYSCALE); // Read the file
 
-    if (image.empty()) // Check for invalid input
-    {
-        cout << "Could not open or find the image" << endl;
-        anyKeyToExit();
-    }
-
     return image;
 }
-
-/*
-Input and clean double quotes ("") from file path
-@return file path
-*/
-string getImageFile()
-{
-    string image_name = "";
-    cout << "Input Image Path: "; getline(cin, image_name);
-
-    if (image_name.substr(0, 1) == "\"" && image_name.substr(image_name.length() - 1, 1) == "\"")
-    {
-        image_name = image_name.substr(1, image_name.length() - 2);
-    }
-
-    return image_name;
-}
-
 
 /*
 Resizes the image while maintaining aspect ratio
@@ -222,16 +238,37 @@ void invertChars(char chars[], const int& CHAR_SIZE)
 
 int main(int argc, char* argv[])
 {
-
-    if (!parseArgs(argc, argv))
-    {
-        return 0;
-    }
-
     Mat image;
     const int CHARS_SIZE = 10; // `~!sTomN@
     char chars[CHARS_SIZE] = { ' ','`','~', '!', 's', 'T', 'o', 'm', 'N' , '@' };
     double y_shrink = 1.956;
+    string image_file;
+    int size = 0;
+    int low = 0;
+    int high = 0;
+    bool black_bg = false;
+    string text_file = "";
+    bool prompt_used = false;
+
+    const parsing result = parseArgs(argc, argv);
+    if (result == parsing::SUCCESS)
+    {
+        image_file = (string)argv[1];
+        size = atoi(argv[2]);
+        low = atoi(argv[3]);
+        high = atoi(argv[4]);
+        black_bg = (bool)atoi(argv[5]);
+        text_file = (string)argv[6];
+    }
+    else if (result == parsing::PROMPT)
+    {
+        promptUserForInput(image_file, size, low, high, black_bg, text_file);
+        prompt_used = true;
+    }
+    else
+    {
+        return 0;
+    }
 
     //int size = 110; // width
     //int low = 60;
@@ -240,13 +277,6 @@ int main(int argc, char* argv[])
     //string image_file "image.jpg";
     //string text_file = "ASCII.txt";
 
-    string image_file = (string)argv[1];
-    int size = atoi(argv[2]);
-    int low = atoi(argv[3]);
-    int high = atoi(argv[4]);
-    bool black_bg = (bool)atoi(argv[5]);
-    string text_file = (string)argv[6];
-
     string ascii = "";
 
     if (!black_bg)
@@ -254,9 +284,19 @@ int main(int argc, char* argv[])
         invertChars(chars, CHARS_SIZE);
     }
 
-    //image_file = getImageFile();
-
     image = readImage(image_file);
+    if (image.empty())
+    {
+        cout << "Failed to open specified image file" << endl;
+        if (prompt_used)
+        {
+            anyKeyToExit();
+        }
+        else
+        {
+            return 0;
+        }
+    }
 
     resizeImage(image, size, y_shrink);
 
@@ -268,7 +308,10 @@ int main(int argc, char* argv[])
 
     saveAscii(ascii, text_file);
 
-    //anyKeyToExit();
+    if (prompt_used)
+    {
+        anyKeyToExit();
+    }
 
     return 0;
 }
